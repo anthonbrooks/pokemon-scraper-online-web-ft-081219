507ace350e5f821140c5e439c6e45b7ead1b896b
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-24 16:24:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  
+  attr_accessor :name, :type
+  attr_reader :id 
+  
+  def initialize(id:nil, name:, type:)
+    @id = id
+    @name = name
+    @type = type
+  end 
+  
+  def self.save
+    
+  end 
+  
+  def self.find 
+    
+  end 
 end
